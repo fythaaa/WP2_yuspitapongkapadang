1dd57a0c0484d6c10a37cd0e5ab7458c123575f5
fythaaa <yuspitapongkapadang07@gmail.com>
2023-03-17 23:03:28 +0700

    web prog

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+
+    echo "<h1>perkenalkan</h1>";
+    echo"Nama saya lestari pita
+        saya tinggal didaerah cengkareng
+        olahraga yang saya sukai adalah rebahan";
+    }
+}
